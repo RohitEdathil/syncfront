9082b8e393c56da13efdebe2728bfda92811bd09
Rohit V <rohitedathil@gmail.com>
2022-11-13 21:43:22 +0530

    Production urls added

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,14 @@
 
-const BackendUrl = "http://localhost:8080/api";
-const WSServerUrl = "ws://localhost:8080/api/ws";
+
+
+const BackendUrl = "https://syncserv.rohitv.repl.co/api"
+const WSServerUrl = "wss://syncserv.rohitv.repl.co/api/ws"
+
+        
+
+// const BackendUrl = "http://localhost:8080/api";
+// const WSServerUrl = "ws://localhost:8080/api/ws";
+
+
 
 export { BackendUrl,WSServerUrl };
